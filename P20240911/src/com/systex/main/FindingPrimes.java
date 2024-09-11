3caf8f4f041db3b186b12d70bffaeee4fc45579c
zoooolander <christina8318@gmail.com>
2024-09-12 00:07:31 +0800

    20240911 homework

--- P20240911/src/com/systex/main/FindingPrimes.java
+++ P20240911/src/com/systex/main/FindingPrimes.java
@@ -4,7 +4,26 @@ public class FindingPrimes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// find out all prime that < 1000
+		System.out.print("2 ");
+		for (int i = 3; i < 1000; i+=2) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+	}
 
+	public static boolean isPrime(int n){
+		if(n<=1){
+			return false;
+		}
+		// 只需檢查到 n 的平方根
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;  // 若能被整除，則不是質數
+            }
+        }
+        return true;
 	}
 
 }
--- P20240911/src/com/systex/main/PrintStar.java
+++ P20240911/src/com/systex/main/PrintStar.java
@@ -6,18 +6,98 @@ public class PrintStar {
 		// TODO Auto-generated method stub
 		int height = 5;
 
-		// loop1
+		// loop1: Right Triangle Star Pattern
+		System.out.println("loop 1");
 		for (int i = 1; i <= height; i++) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
+		System.out.println();
 
 		// loop2
-		for (int i = height; i >= 1; i--) {
-
+		System.out.println("loop 2");
+		for (int i = 1; i <= height; i++) {
+			for (int j = height; j >= i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
+		System.out.println();
+
+		// loop3
+		System.out.println("loop 3");
+		for (int i = 1; i <= height; i++) {
+			for (int j = height - 1; j >= i; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		// loop4: Pyramid Star Pattern
+		System.out.println("loop 4");
+		for (int i = 1; i <= height; i++) {
+			for (int j = height - 1; j >= i; j--) {
+				System.out.print(" ");
+			}
+			// print (2*height-1) star
+			for (int j = 1; j <= 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		// loop 5
+		for (int i = 1; i <= height; i++) {
+			for (int j = 1; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = height; j >= i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		// loop 6
+		for (int i = 1; i <= height; i++) {
+			for (int j = 1; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= 2 * (height - i + 1) - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		//loop 7: Diamond Shape Pattern
+		for (int i = 1; i <= height; i++) {
+			//上半部分
+            for (int j = height - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // 下半部分
+        for (int i = height - 1; i >= 1; i--) {
+            for (int j = height - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 	}
 
 }
